4db4d179318150e9c8623c0ee5c67e5266b78a99
Dave Smith <davewsmith@gmail.com>
2021-10-24 19:47:24 -0700

    Migrations cleanup

--- app/toy/models.py
+++ app/toy/models.py
@@ -3,5 +3,5 @@ from app import db
 
 class ToyData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    posted_ad = db.Column(db.DateTime, index=True)
+    created_at = db.Column(db.DateTime, index=True)
     toy_data = db.Column(db.String(255))
