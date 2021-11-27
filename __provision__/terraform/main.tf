92eeec7fea2af45bf1ff8ff30c2aade1b1b68713
Dave Smith <davewsmith@gmail.com>
2021-11-27 15:52:17 -0800

    Refactor main.tf (#4)

--- __provision__/terraform/main.tf
+++ __provision__/terraform/main.tf
@@ -12,52 +12,42 @@ provider "aws" {
   region  = "us-west-1"
 }
 
-variable "cidr_vpc" {
-  description = "CIDR block for VPC"
-  default     = "10.1.0.0/16"
-}
-
-variable "cidr_subnet" {
-  description = "CIDR block for subnet"
-  default     = "10.1.0.0/20"
-}
-
-resource "aws_vpc" "toydeploy-vpc" {
+resource "aws_vpc" "app-vpc" {
   cidr_block           = var.cidr_vpc
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "toydeploy-vpc"
+    Name = "${var.project-name}-vpc"
   }
 }
 
-resource "aws_internet_gateway" "toydeploy-ig" {
-  vpc_id = aws_vpc.toydeploy-vpc.id
+resource "aws_internet_gateway" "app-ig" {
+  vpc_id = aws_vpc.app-vpc.id
 }
 
-resource "aws_route_table" "toydeploy-rt" {
-  vpc_id = aws_vpc.toydeploy-vpc.id
+resource "aws_route_table" "app-rt" {
+  vpc_id = aws_vpc.app-vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.toydeploy-ig.id
+    gateway_id = aws_internet_gateway.app-ig.id
   }
 }
 
-resource "aws_subnet" "toydeploy-subnet" {
-  vpc_id                  = aws_vpc.toydeploy-vpc.id
+resource "aws_subnet" "app-subnet" {
+  vpc_id                  = aws_vpc.app-vpc.id
   cidr_block              = var.cidr_subnet
   map_public_ip_on_launch = true
 }
 
-resource "aws_route_table_association" "toydeploy-rta" {
-  subnet_id      = aws_subnet.toydeploy-subnet.id
-  route_table_id = aws_route_table.toydeploy-rt.id
+resource "aws_route_table_association" "app-rta" {
+  subnet_id      = aws_subnet.app-subnet.id
+  route_table_id = aws_route_table.app-rt.id
 }
 
-resource "aws_security_group" "toydeploy-sg" {
-  name   = "toydeploy-sg"
-  vpc_id = aws_vpc.toydeploy-vpc.id
+resource "aws_security_group" "app-sg" {
+  name   = "${var.project-name}-sg"
+  vpc_id = aws_vpc.app-vpc.id
 
   ingress {
     from_port = 22
@@ -86,14 +76,14 @@ resource "aws_security_group" "toydeploy-sg" {
   }
 }
 
-resource "aws_instance" "toydeploy" {
+resource "aws_instance" "app-appserver" {
   ami             = "ami-083f68207d3376798" # Ubuntu 18.04
   instance_type   = "t2.micro"
-  security_groups = ["${aws_security_group.toydeploy-sg.id}"]
-  subnet_id       = aws_subnet.toydeploy-subnet.id
-  key_name        = "toydeploy"
+  security_groups = ["${aws_security_group.app-sg.id}"]
+  subnet_id       = aws_subnet.app-subnet.id
+  key_name        = var.key-name
 
   tags = {
-    Name = "Toy Deploy"
+    Name = "${var.project-name}_appserver"
   }
 }
