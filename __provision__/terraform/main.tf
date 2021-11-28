c62cdb0d0447b3e1d071d2f726c11dcca6cdff14
Dave Smith <davewsmith@gmail.com>
2021-11-27 20:15:01 -0800

    Add ingress for ping and https

--- __provision__/terraform/main.tf
+++ __provision__/terraform/main.tf
@@ -16,6 +16,15 @@ resource "aws_security_group" "app-sg" {
   name = "app-sg"
 
   ingress {
+    from_port = 8
+    to_port   = 0
+    protocol  = "icmp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
@@ -32,6 +41,16 @@ resource "aws_security_group" "app-sg" {
       "0.0.0.0/0"
     ]
   }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
 
   # Terraform removes the default rule, so we re-add it.
   egress {
